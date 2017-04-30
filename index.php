65f2852e742170d4d6f7aa57edc8d5d9f9d5f282
abombin <bombin78@mail.ru>
2017-04-30 21:13:59 +0300

    Created index.php, connected twig.php, fixed modal

---
+++ index.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ссылки на страницы</title>
+	<meta charset="utf-8">
+	<meta name="keywords" content="">
+	<meta name="description" content="">
+	<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/vnd.microsoft.icon" href="frontend/favicon.ico">
+	<style>
+		body {
+			text-align : center;
+		}
+
+		h1 {
+			font-size   : 20px;
+			font-weight : bold;
+			padding     : 15px;
+		}
+
+		ul {
+			width        : 500px;
+			margin       : 0 auto;
+			padding-left : 0;
+
+			text-align   : left;
+			list-style   : none;
+		}
+
+		li {
+			margin-top : 10px;
+		}
+
+		.link {
+			display: block;
+		}
+	</style>
+</head>
+<body>
+<h1>Ссылки на страницы сайта</h1>
+<ul>
+	<li>
+		0. Инструкция
+		<a class="link" href="frontend/guide/guide.html" target="_blank">
+			0.1 Kомпоненты
+		</a>
+		<a class="link" href="frontend/page-base.html" target="_blank">
+			0.2 Шаблон (базовый - используется для корзины)
+		</a>
+		<a class="link" href="frontend/page-left.html" target="_blank">
+			0.3 Шаблон (двухколоночный с левым сайдбаром - основной шаблон)
+		</a>
+		<a class="link" href="frontend/page-right.html" target="_blank">
+			0.4 Шаблон (двухколоночный с правым сайдбаром - дополнительно)
+		</a>
+		<a class="link" href="frontend/page-one.html" target="_blank">
+			0.5 Шаблон (одноколоночный - дополнительно)
+		</a>
+	</li>
+	<li>
+		1. Главная страница
+		<a class="link" href="http://manicurshop.ru/" target="_blank">
+			1.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/index.php" target="_blank">
+			1.2 Новый вариант
+		</a>
+		<a class="link" href="frontend/index-guest.html" target="_blank">
+			1.3 Новый вариант (для гостя + акция)
+		</a>
+	</li>
+	<li>
+		2. Каталог товаров
+		<a class="link" href="http://manicurshop.ru/market/" target="_blank">
+			2.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/catalog.html" target="_blank">
+			2.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		3. Предложения
+		<a class="link" href="http://manicurshop.ru/market/manicurekit/" target="_blank">
+			3.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/offers.html" target="_blank">
+			3.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		4. Карточка предложения
+		<a class="link" href="http://manicurshop.ru/market/manikyurnyy-nabor-chemodan-zinger-ms-bg/" target="_blank">
+			4.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/card.html" target="_blank">
+			4.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		5. Карзина + шаги оформления заказа
+		<a class="link" href="http://manicurshop.ru/personal/cart/" target="_blank">
+			5.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/basket-step1.html" target="_blank">
+			5.2 Новый вариант - корзина + шаг 1
+		</a>
+		<a class="link" href="frontend/basket-step2-1.html" target="_blank">
+			5.3 Новый вариант - шаг 2.1
+		</a>
+		<a class="link" href="frontend/basket-step2-2.html" target="_blank">
+			5.4 Новый вариант - шаг 2.2
+		</a>
+	</li>
+	<li>
+		6. Личный кабинет
+		<a class="link" href="http://manicurshop.ru/personal/profile/" target="_blank">
+			6.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/personal.html" target="_blank">
+			6.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		7. О магазине
+		<a class="link" href="http://manicurshop.ru/about/" target="_blank">
+			7.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/about-store.html" target="_blank">
+			7.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		8. О доставке и оплате
+		<a class="link" href="http://manicurshop.ru/delivery/" target="_blank">
+			8.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/about-purchase.html" target="_blank">
+			8.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		9. Помощь
+		<a class="link" href="http://manicurshop.ru/faq/" target="_blank">
+			9.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/faq.html" target="_blank">
+			9.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		10. Контакты
+		<a class="link" href="http://manicurshop.ru/contacts/" target="_blank">
+			10.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/contacts.html" target="_blank">
+			10.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		11. Публичная оферта
+		<a class="link" href="http://manicurshop.ru/oferta/" target="_blank">
+			11.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/contract.html" target="_blank">
+			11.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		12. Теги
+		<a class="link" href="http://manicurshop.ru/tags/" target="_blank">
+			12.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/tags.html" target="_blank">
+			12.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		13. Страница тега
+		<a class="link" href="http://manicurshop.ru/tags/vosk-dlya-depilyatsii/" target="_blank">
+			13.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/tag-page.html" target="_blank">
+			13.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		14. Карта сайта
+		<a class="link" href="http://manicurshop.ru/sitemap/" target="_blank">
+			14.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/sitemap.html" target="_blank">
+			14.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		15. Мастерская по заточке
+		<a class="link" href="http://manicurshop.ru/masterskaya_po_zatochke/" target="_blank">
+			15.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/workshop.html" target="_blank">
+			15.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		16. Блог
+		<a class="link" href="http://manicurshop.ru/blog/" target="_blank">
+			16.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/blog.html" target="_blank">
+			16.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		17. Статьи блога
+		<a class="link" href="http://manicurshop.ru/blog/dizayn-nogtey-moscow-street/" target="_blank">
+			17.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/blog-page.html" target="_blank">
+			17.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		18. Список видео
+		<a class="link" href="http://manicurshop.ru/video/" target="_blank">
+			18.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/video.html" target="_blank">
+			18.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		19. Страница с видео
+		<a class="link" href="http://manicurshop.ru/video/video-zatochka-manikyurnykh-kusachek/" target="_blank">
+			19.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/video-page.html" target="_blank">
+			19.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		20. Страница восстановления пароля
+		<a class="link" href="http://manicurshop.ru/login/?forgot_password=yes" target="_blank">
+			20.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/password-recover.html" target="_blank">
+			20.2 Новый вариант
+		</a>
+	</li>
+	<li>
+		21. Страница смены пароля
+		<a class="link" href="http://manicurshop.ru/personal/profile/?change_password=yes" target="_blank">
+			21.1 Вид страницы на сайте
+		</a>
+		<a class="link" href="frontend/password-change.html" target="_blank">
+			21.2 Новый вариант
+		</a>
+	</li>
+</ul>
+</body>
+</html>
