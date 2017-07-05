373acc528cb577c9d0dd00695ccd13775b82d411
abombin <bombin78@mail.ru>
2017-07-05 22:57:53 +0300

    fixed common.js

--- frontend/js/common.js
+++ frontend/js/common.js
@@ -30,8 +30,8 @@
 //------------------------------------------------------------
 
 //------------------------------------------------------------
-// МОДУЛЬ ПОКАЗАТЬ/СКРЫТЬ МЕНЮ 2-ГО УРОВНЯ В МОБИЛЬНОЙ ВЕРСИИ.
-$('.su-level1._su-list').on('click', function (e) {
+// МОДУЛЬ ПОКАЗАТЬ/СКРЫТЬ ВЛОЖЕННОЕ МЕНЮ КАТАЛОГА В МОБИЛЬНОЙ ВЕРСИИ.
+$('._su-list').on('click', function (e) {
 	e.preventDefault();
 	var wrapper = $(this).closest('.su-list_item'),
 		wrapperModifier = '_su-open',
@@ -42,31 +42,6 @@ $('.su-level1._su-list').on('click', function (e) {
 	if (wrapper.hasClass(wrapperModifier)) {
 		description.slideUp(duration, function () {
 			wrapper.removeClass(wrapperModifier);
-			description.removeAttr(wrapperAttr);
-		});
-	}
-	else {
-		description.slideDown(duration, function () {
-			wrapper.addClass(wrapperModifier);
-			description.removeAttr(wrapperAttr);
-		});
-	}
-});
-//------------------------------------------------------------
-
-//------------------------------------------------------------
-// МОДУЛЬ ПОКАЗАТЬ/СКРЫТЬ МЕНЮ 3-ГО УРОВНЯ В МОБИЛЬНОЙ ВЕРСИИ.
-$('.su-level2._su-list').on('click', function (e) {
-	e.preventDefault();
-	var wrapper = $(this).closest('.su-list_item'),
-		wrapperModifier = '_su-open',
-		wrapperAttr = 'style',
-		description = wrapper.find('.su-dropdown'),
-		duration = 200;
-
-	if (wrapper.hasClass(wrapperModifier)) {
-		wrapper.removeClass(wrapperModifier);
-		description.slideUp(duration, function () {
 			description.removeAttr(wrapperAttr);
 		});
 	}
@@ -105,36 +80,6 @@ var toggleUser = new ToggleShow();
 		 modifierNameDrop: '_su-show'
 	 }
  );
-//------------------------------------------------------------
-
-//------------------------------------------------------------
-// КАТАЛОГ МОБИЛЬНОЙ ВЕРСИИ САЙТА
-// ИНИЦИАЛИЗАЦИЯ 1-ГО УРОВНЯ
-// var toggleCatalogLevel1 = new ToggleShow();
-// toggleCatalogLevel1.init(
-// 	{
-// 		closeDropdown: 'otherClose',
-// 		classNameLevel: 'su-level1',
-// 		classNameMain: 'su-list_item',
-// 		classNameOpen: 'su-list_link',
-// 		classNameDrop: 'su-list_dropdown',
-// 		modifierNameOpen: '_su-open',
-// 		modifierNameDrop: '_su-show'
-// 	}
-// );
-// // ИНИЦИАЛИЗАЦИЯ 2-ГО УРОВНЯ
-// var toggleCatalogLevel2 = new ToggleShow();
-// toggleCatalogLevel2.init(
-// 	{
-// 		closeDropdown: 'otherClose',
-// 		classNameLevel: 'su-level2',
-// 		classNameMain: 'su-list_item',
-// 		classNameOpen: 'su-list_link',
-// 		classNameDrop: 'su-list_dropdown',
-// 		modifierNameOpen: '_su-open',
-// 		modifierNameDrop: '_su-show'
-// 	}
-// );
 //------------------------------------------------------------
 
 // УСТРАНЕНИЕ ЭФФЕКТА СМЕЩЕНИЯ ШАПКИ ПРИ ОТКРЫТИИ МОДАЛЬНОГО ОКНА
